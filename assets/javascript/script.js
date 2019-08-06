e40f670a6293f3ac1093c1a7c3717ffcb5a04ca0
brandnn <brandnn@uw.edu>
2019-08-06 12:55:56 -0700

    getting firebase set up

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBg6IyWj3curcfPuAeV8MMy07PgO1WuTjY",
+    authDomain: "first-project-acab1.firebaseapp.com",
+    databaseURL: "https://first-project-acab1.firebaseio.com",
+    projectId: "first-project-acab1",
+    storageBucket: "first-project-acab1.appspot.com",
+    messagingSenderId: "1027238424289",
+    appId: "1:1027238424289:web:0cc4354c688c9a31"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
